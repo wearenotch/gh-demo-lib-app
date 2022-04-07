b38c3ab9c819e879ba22ec49f5e7d65f664f7a76
Domagoj Madunić <domagoj.madunic@ag04.com>
2022-04-07 10:49:40 +0200

    Setup build and added initial Java classes

---
+++ src/main/java/com/ag04/testlibdemo/TestLibDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ag04.testlibdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLibDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLibDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ag04/testlibdemo/TestLibDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag04.testlibdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLibDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
